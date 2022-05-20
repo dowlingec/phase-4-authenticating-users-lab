a518ba4b4488bc130c577ee18abebaeafd4cc7f4
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-20 14:49:10 -0400

    routes written

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
+
+  post '/login', to: 'SessionsController#create'
+  delete '/logout', to: 'SessionController#destroy'
+
+  get '/me', to: 'UsersController#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
