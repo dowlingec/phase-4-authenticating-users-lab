c84299935c76fc491f1a392e0577aa2ad7b4766a
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-30 15:05:34 -0400

    commit tree ae61fb809d4b434e0ceee64dea577ab36917702e

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+    
+    #LOGIN
+    def create
+        user = User.find_by(username: params[:username])
+        session[:user_id] = user.id
+        render json: user
+    end
+    
+    #LOGOUT
+    def destroy
+        session.delete :user_id
+        head :no_content
+      end
+  end
